715cb895fac131629078485fb905d3ab30797de8
Collin Kruger <collin.kruger@live.com>
2016-11-27 08:15:17 -0600

    Initial

---
+++ CK.System.Collections.Generic/HashSetExt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CK.System.Collections.Generic
+{
+    /// <summary>
+    /// Enumables creating <see cref="HashSet{T}"/> from <see cref="IEnumerable{T}"/> via extension methods
+    /// </summary>
+    public static class HashSetExt
+    {
+        /// <summary>
+        /// Calls <see cref="HashSet{T}" /> constructor
+        /// </summary>
+        public static HashSet<T> ToHashSet<T>(IEnumerable<T> items) => new HashSet<T>(items);
+
+        /// <summary>
+        /// Calls <see cref="HashSet{T}" /> constructor
+        /// </summary>
+        public static HashSet<T> ToHashSet<T>(IEnumerable<T> items, IEqualityComparer<T> comparer) => new HashSet<T>(items, comparer);
+    }
+}
---
+++ CK.System.Collections.Generic/KVP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.System.Collections.Generic
+{
+    /// <summary>
+    /// Static helper for dealing with <see cref="KeyValuePair{TKey, TValue}"/> 
+    /// </summary>
+    public static class KVP
+    {
+        /// <summary>
+        /// Enumables creating a <see cref="KeyValuePair{TKey, TValue}"/> without having to specify type parameters
+        /// </summary>
+        public static KeyValuePair<TKey, TValue> Gen<TKey, TValue>(TKey key, TValue value) => new KeyValuePair<TKey, TValue>(key, value);
+    }
+}
